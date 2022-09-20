eaae693e092d8b2b7c54afbaa3f414d49d8b545c
hassanaftab93 <hassanaftab.pk@gmail.com>
2022-09-20 23:46:25 +0500

    Download path added

--- src/download.py
+++ src/download.py
@@ -1,19 +1,32 @@
 # This script takes a link as an input and outputs the downloaded file into the specified folder
 
+from time import sleep
 from pytube import YouTube
 from sys import argv
 import os
 
-link = argv[1]
+#link = argv[1]
+link = 'http://www.youtube.com/watch?v=3HFBR0UQPes'
+
+if os.name == "nt":
+    DOWNLOAD_FOLDER = f"{os.getenv('USERPROFILE')}\\Downloads"
+else:  # PORT: For unix systems
+    DOWNLOAD_FOLDER = f"{os.getenv('HOME')}/Downloads"
 
 if link!="":
     yt = YouTube(link)
     print("\nTitle: ", yt.title)    # Title of Video
     print("\nView: ", yt.views)     # Views of Video
     print("\nAuthor: ",yt.author)   # Name of Author
-    yd = yt.streams.get_highest_resolution()
-    
-    # ADD DOWNLOAD FOLDER HERE
-    yd.download('$PATH')
+
+    yd = yt.streams.get_highest_resolution() # Download Object set to highest res.
+
+    print("\nPath of Download Folder: ",DOWNLOAD_FOLDER)
+    print("\n\nVideo: Downloading...\n\n")
+
+    yd.download(DOWNLOAD_FOLDER)
+    sleep(2000)
+    print("\n\nVideo: Downloaded\n\n")
+
 elif link=="":
     os.system("sh ../yt.sh")
