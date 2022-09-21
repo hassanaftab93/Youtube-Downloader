b356ae07b14a522afbdf36b7f74fc17fe3d775ab
hassanaftab93 <hassanaftab.pk@gmail.com>
2022-09-21 20:45:29 +0500

    Made changes to code structure

--- yt.sh
+++ yt.sh
@@ -2,16 +2,17 @@
 
 # This shell script executes the python file
 # This is to make the python file executable
-chmod u+x ./src/download.py
+chmod u+x ./src/main.py
 
 python -m venv venv
 
 source ./venv/bin/activate
-source venv/Scripts/activate
+# source venv/Scripts/activate
 
 pip install -r requirements.txt
 python -m pip install --upgrade pytube
 clear
+
 echo ""
 echo "Please Enter a Valid Youtube Video Link or Press CTRL+C to Exit: "
 echo ""
@@ -19,7 +20,5 @@ read url_input
 echo ""
 echo "Processing.."
 echo ""
-# Refactor download.py into a function
-# Variable = `Command to call download.py`
 
-python ./src/download.py "$url_input"
+python ./src/main.py "$url_input"
