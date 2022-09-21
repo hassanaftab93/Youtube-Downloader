9d64af1580e7d09d3b0fb3ae736f74b72e750527
hassanaftab93 <hassanaftab.pk@gmail.com>
2022-09-21 22:37:31 +0500

    Code prepped for executable conversion

---
+++ src/download.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# This script takes a link as an input and outputs the downloaded file into the specified folder
+
+from time import sleep
+from pytube import YouTube
+from sys import argv
+import os
+
+def getTitle(ytObject):
+    return ytObject.title
+
+def getViews(ytObject):
+    return ytObject.views
+
+def getAuthor(ytObject):
+    return ytObject.author
+
+def downloadVideo(ytObject,link,Path):
+
+    yd = ytObject.streams.get_highest_resolution() # Download Object set to highest res.
+    print("\n\nVideo: Downloading...\n\n")
+    yd.download(Path)
+    sleep(5)
+    print("\n\nVideo: Download Complete\n\n")
+    sleep(5)
+
+print("\nYoutube Downloader\n")
+
+link=str(input("Please Enter a Valid Youtube Video Link or Press CTRL+C to Exit: "))
+
+if link!="":
+    yt = YouTube(link) # we will pass this object into function -> functions.py
+    print("\nVideo Title: ",getTitle(yt))
+    print("\nVideo Author: ",getAuthor(yt))
+    print("\nVideo Views: ",getViews(yt))
+
+    if os.name == "nt":
+        DOWNLOAD_FOLDER = f"{os.getenv('USERPROFILE')}\\Downloads"
+    else:  # PORT: For unix systems
+        DOWNLOAD_FOLDER = f"{os.getenv('HOME')}/Downloads"
+
+    downloadVideo(yt,link,DOWNLOAD_FOLDER)
+
+else:
+    exit
